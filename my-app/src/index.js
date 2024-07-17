c78571791cdde8cff3d172317872e30272ceec3e
AL2002MI08 <alexandramizero@gmail.com>
2024-07-17 16:13:48 +0200

    using custom components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -61,27 +61,22 @@
 //     </div>,
 //     document.getElementById("root")
 // )
-import React from "react"
-import ReactDOM from "react-dom"
 
-/**
-Challenge: fix our code!
-
-Don't forget, you're not using CDNs anymore, so there's no
-global "ReactDOM" variable to use anymore.
- */
-
-const page = (
+// import React from 'react'
+import ReactDOM from "react-dom";
+function customComponents() {
+  return (
     <div>
-        <h1>My awesome website in React</h1>
-        <h3>Reasons I love React</h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's a hireable skill</li>
-            <li>It's actively maintained by skilled people</li>
-        </ol>
+      <img src="react-logo.png" height="50px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Was well over 100k stars</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powered by thousands apps including mobile apps</li>
+      </ul>
     </div>
-)
-
-ReactDOM.render(page, document.getElementById("root"))
+  );
+}
+ReactDOM.render(customComponents(), document.getElementById("root"));
