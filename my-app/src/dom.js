860e76d632a6d40476ac4d9e9b6156dd208536c1
AL2002MI08 <alexandramizero@gmail.com>
2024-07-17 14:54:02 +0200

    using ReactDOM

---
+++ my-app/src/dom.js
@@ -0,0 +1,14 @@
+// const element = document.getElementById('root')
+// const newElement = document.createElement('h1')
+// newElement.className = 'heading'
+// newElement.textContent = 'React Basics'
+// element.appendChild(newElement)
+// let page = (
+//     <div>
+//         <h1>Using JSX</h1>
+//         <p>Paragraph</p>
+//     </div>
+// )
+// console.log(page)
+// ReactDOM.render(page, document.getElementById('root'))
+
