94caaa7552682ab30f223bd495ae18cee6a8f3d1
AL2002MI08 <alexandramizero@gmail.com>
2024-07-18 20:02:11 +0200

    create new file in src

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,101 +1,54 @@
-// // ReactDOM.render(<p>Fun facts about react</p>, document.getElementById('root'))
-// // ReactDOM.render(<ul><li>car brand</li><li>car price</li></ul>, document.getElementById('root'))
+import React from "react"
+import ReactDOM from "react-dom"
 
-// function Navbar() {
-//     return (
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//             <a className="navbar-brand" href="#">Navbar</a>
-//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>
+/**
+Challenge: 
 
-//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                 <ul className="navbar-nav mr-auto">
-//                 <li className="nav-item active">
-//                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-//                 </li>
-//                 <li className="nav-item">
-//                     <a className="nav-link" href="#">Link</a>
-//                 </li>
-//                 <li className="nav-item dropdown">
-//                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                     Dropdown
-//                     </a>
-//                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                     <a className="dropdown-item" href="#">Action</a>
-//                     <a className="dropdown-item" href="#">Another action</a>
-//                     <div className="dropdown-divider"></div>
-//                     <a className="dropdown-item" href="#">Something else here</a>
-//                     </div>
-//                 </li>
-//                 <li className="nav-item">
-//                     <a className="nav-link disabled" href="#">Disabled</a>
-//                 </li>
-//                 </ul>
-//                 <form className="form-inline my-2 my-lg-0">
-//                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-//                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//                 </form>
-//             </div>
-//         </nav>
-//     )
-// }
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
 
-// // Challenge: Create your own custom React component!
-// // Call it "MainContent", and have it return a simple
-// // h1 element that says "I'm learning React!"
-
-// // Afterward, render it below the Navbar (which
-// // you can do inside the ReactDOM.render call below)
-
-// function MainContent() {
-//     return (
-//         <h1>I am learning React!</h1>
-//     )
-// }
-
-// ReactDOM.render(
-//     <div>
-//         <Navbar />
-//         <MainContent />
-//     </div>,
-//     document.getElementById("root")
-// )
-
-import React from 'react'
-import ReactDOM from "react-dom";
-// function CustomComponents() {
-//   return (
-//     <div>
-//       <img src="react-logo.png" height="50px" />
-//       <h1>Fun facts about React</h1>
-//       <ul>
-//         <li>Was first released in 2013</li>
-//         <li>Was originally created by Jordan Walke</li>
-//         <li>Was well over 100k stars</li>
-//         <li>Is maintained by Facebook</li>
-//         <li>Powered by thousands apps including mobile apps</li>
-//       </ul>
-//     </div>
-//   );
-// }
-function Page() {
-  return (
-    <div>
-      <header>
-        <nav>
-          <img src="react-logo.png" width="80px" />
-        </nav>
-      </header>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
-      <footer><small>© 2024 Kellma development. All rights reserved.</small></footer>
-    </div>
-  );
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
 }
-ReactDOM.render(<Page />, document.getElementById("root"));
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+ReactDOM.render(<Page />, document.getElementById('root'))
