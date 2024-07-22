ee598ed479c7e2815c0b34b464301d3a619c0a3f
AL2002MI08 <alexandramizero@gmail.com>
2024-07-22 11:02:12 +0200

    applying map method

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,16 +6,46 @@ import "./App.css";
 import { Navbar } from "../components/Navbar.jsx";
 import { Main } from "../components/Main.jsx";
 import { Page } from "../components/Page.jsx";
-function App() {
-  return (
-    <>
-      {/* <Header />
-      <Description />
-      <Footer /> */}
-      {/* <Navbar />
-      <Main /> */}
-      <Page />
-    </>
-  );
+// function App() {
+//   return (
+//     <>
+//       {/* <Header />
+//       <Description />
+//       <Footer /> */}
+//       {/* <Navbar />
+//       <Main /> */}
+//       <Page />
+//     </>
+//   );
+// }
+// export default App;
+import React from "react"
+import { Contact } from "../components/Contact"
+import Card from "../components/Card";
+import data from "../data.jsx";
+
+export default function App() {
+    const cards = data.map((item, index) => {
+        return (
+        <Card 
+        key= {item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        />
+        )
+    })
+    return (
+        // 
+        //<Card />
+        <>
+        <Navbar />
+        {cards}
+        </>
+        
+    )
 }
-export default App;
+
