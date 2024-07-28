d663c445e2a00d0dd29b016a7667675d220f9cae
AL2002MI08 <alexandramizero@gmail.com>
2024-07-28 21:04:02 +0200

    use useEffect cleanup function

--- today-journal/Journal-today/counter/App.jsx
+++ today-journal/Journal-today/counter/App.jsx
@@ -30,4 +30,19 @@ export default function App() {
             <button className="counter--plus" onClick={add}>+</button>
         </div>
     )
+// windowSize tracker and how to handle event listeners in useEffect
+    // const [show, setShow] = React.useState(true)
+    
+    // function toggle() {
+    //     setShow(prevShow => !prevShow)
+    // }
+    
+    // return (
+    //     <div className="container">
+    //         <button onClick={toggle}>
+    //             Toggle WindowTracker
+    //         </button>
+    //         {show && <WindowTracker />}
+    //     </div>
+    // )
 }
