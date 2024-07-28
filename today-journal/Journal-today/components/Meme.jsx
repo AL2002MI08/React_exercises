388cdfe38aa1ce55a2971c0c87570dd3e90dfa53
AL2002MI08 <alexandramizero@gmail.com>
2024-07-28 20:53:06 +0200

    using async function inside useEffect hook

--- today-journal/Journal-today/components/Meme.jsx
+++ today-journal/Journal-today/components/Meme.jsx
@@ -1,35 +1,31 @@
 import React from "react"
-import memesData from "../memesData.js"
 
 export default function Meme() {
-    /**
-     * Challenge: 
-     * As soon as the Meme component loads the first time,
-     * make an API call to "https://api.imgflip.com/get_memes".
-     * 
-     * When the data comes in, save just the memes array part
-     * of that data to the `allMemes` state
-     * 
-     * Think about if there are any dependencies that, if they
-     * changed, you'd want to cause to re-run this function.
-     * 
-     * Hint: for now, don't try to use an async/await function.
-     * Instead, use `.then()` blocks to resolve the promises
-     * from using `fetch`. We'll learn why after this challenge.
-     */
-    
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
     const [allMemes, setAllMemes] = React.useState([])
-    React.useEffect(()=>{
-        fetch("https://api.imgflip.com/get_memes")
-        .then(resp => resp.json())
-        .then(data => setAllMemes(data.data.memes))
-    },[])
     
+    /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+    
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
     
     function getMemeImage() {
         const randomNumber = Math.floor(Math.random() * allMemes.length)
@@ -38,7 +34,6 @@ export default function Meme() {
             ...prevMeme,
             randomImage: url
         }))
-        
     }
     
     function handleChange(event) {
